865af5e5ec0655c476b2fd67335a30ab1015947d
idks2022 <idankashi2007@gmail.com>
2022-08-23 19:18:31 +0300

    aws functions writing started

---
+++ cuinclass/core/fr/awsfuncs.py
@@ -0,0 +1,122 @@
+import boto3
+from botocore.exceptions import ClientError
+from os import environ
+
+def create_collection(collection_id):
+    client=boto3.client('rekognition')
+
+    #Create a collection
+    print('Creating collection:' + collection_id)
+    response=client.create_collection(CollectionId=collection_id)
+    print('Collection ARN: ' + response['CollectionArn'])
+    print('Status code: ' + str(response['StatusCode']))
+    print('Done...')
+    
+
+def list_collections():
+
+    max_results=2
+    
+    client=boto3.client('rekognition')
+
+    #Display all the collections
+    print('Displaying collections...')
+    response=client.list_collections(MaxResults=max_results)
+    collection_count=0
+    done=False
+    
+    while done==False:
+        collections=response['CollectionIds']
+
+        for collection in collections:
+            print (collection)
+            collection_count+=1
+        if 'NextToken' in response:
+            nextToken=response['NextToken']
+            response=client.list_collections(NextToken=nextToken,MaxResults=max_results)
+            
+        else:
+            done=True
+    #return collection_count 
+    print("collections: " + str(collection_count))
+
+
+def describe_collection(collection_id):
+
+    print('Attempting to describe collection ' + collection_id)
+    client=boto3.client('rekognition')
+
+    try:
+        response=client.describe_collection(CollectionId=collection_id)
+        print("Collection Arn: "  + response['CollectionARN'])
+        print("Face Count: "  + str(response['FaceCount']))
+        print("Face Model Version: "  + response['FaceModelVersion'])
+        print("Timestamp: "  + str(response['CreationTimestamp']))
+
+        
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print ('The collection ' + collection_id + ' was not found ')
+        else:
+            print ('Error other than Not Found occurred: ' + e.response['Error']['Message'])
+    print('Done...')
+
+
+def delete_collection(collection_id):
+
+
+    print('Attempting to delete collection ' + collection_id)
+    client=boto3.client('rekognition')
+    status_code=0
+    try:
+        response=client.delete_collection(CollectionId=collection_id)
+        status_code=response['StatusCode']
+        
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print ('The collection ' + collection_id + ' was not found ')
+        else:
+            print ('Error other than Not Found occurred: ' + e.response['Error']['Message'])
+        status_code=e.response['ResponseMetadata']['HTTPStatusCode']
+    # return(status_code)
+    print('Status code: ' + str(status_code))
+
+
+def add_faces_to_collection(bucket,photo,collection_id):
+
+    client=boto3.client('rekognition')
+
+    response=client.index_faces(CollectionId=collection_id,
+                                Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+                                ExternalImageId=photo,
+                                MaxFaces=1,
+                                QualityFilter="AUTO",
+                                DetectionAttributes=['ALL'])
+
+    print ('Results for ' + photo) 	
+    print('Faces indexed:')						
+    for faceRecord in response['FaceRecords']:
+         print('  Face ID: ' + faceRecord['Face']['FaceId'])
+         print('  Location: {}'.format(faceRecord['Face']['BoundingBox']))
+
+    print('Faces not indexed:')
+    for unindexedFace in response['UnindexedFaces']:
+        print(' Location: {}'.format(unindexedFace['FaceDetail']['BoundingBox']))
+        print(' Reasons:')
+        for reason in unindexedFace['Reasons']:
+            print('   ' + reason)
+    print("Faces indexed count: " + str('FaceRecords'))
+    # return len(response['FaceRecords'])
+
+
+def main():
+    create_collection('studentsCollection')
+    list_collections()
+    # describe_collection('Collection')
+    # delete_collection('Collection')
+    add_faces_to_collection('cuinclass','cuinclass\core\fr\facesToCollection\Idan.jpg','studentsCollection')
+
+
+
+if __name__ == "__main__":
+    main()
